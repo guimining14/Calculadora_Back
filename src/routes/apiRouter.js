53f22c8e19436e28a1af5d50f304e1c129814ee8
Guilherme <guilherme.carletti.mining@gmail.com>
2022-05-29 16:32:20 -0300

    exclusão do node_modules

--- src/routes/apiRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const apiController = require('../controllers/apiController')
-
-router.post('/login', apiController.login)
-router.get('/logoff', apiController.logoff)
-
-module.exports = router
--- src/routes/categoriaRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const categoriaController = require('../controllers/categoriaController')
-
-router.get('/', categoriaController.listar)
-
-module.exports = router
